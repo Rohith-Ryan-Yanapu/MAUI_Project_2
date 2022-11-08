5916e91a676dc6c059d29cb20158f7cf5c71b1ce
prudhvireddymali <prudhvireddymali2021@gmail.com>
2022-11-08 16:22:05 -0600

    feat: Expression Generator

--- MAUI-Main-APP/src/Calculator/Views/MathQuiz.xaml.cs
+++ MAUI-Main-APP/src/Calculator/Views/MathQuiz.xaml.cs
@@ -1,43 +1,83 @@
-
 namespace Calculator;
 
 public partial class MathQuiz : ContentPage
 {
-    int opt1, opt2, opt3;
-
+    int Score = 0;
     Random random = new Random();
+    int var1, var2, opt1 = 0, opt2 = 0, opt3 = 0, exp = -1;
+    String expression;
     public MathQuiz()
-	{
-		InitializeComponent();
-	}
+    {
+        InitializeComponent();
+    }
 
     void Started(object sender, EventArgs e)
     {
         visibleGame(1);
         generateQuestion();
+        this.ScoreValue.Text = Score.ToString();
     }
 
     void restart(object sender, EventArgs e)
     {
+        Score = 0;
         visibleGame(0);
+        this.ScoreValue.Text = Score.ToString();
     }
+
 
     void generateQuestion()
     {
-        getExpression(); 
+        expression = getExpression();
         giveOptions();
     }
 
-    void getExpression()
+    void clickedTryAgain(object sender, EventArgs e)
     {
-        int var1,var2;
+        
+    }
+
+    void clickedNext(object sender, EventArgs e)
+    {
+        
+
+    }
+    
+    String getExpression()
+    {
+        var1 = 1; var2 = 100;
         String allExpressions = "+-/*";
-        var1 = random.Next(1, 10);
-        var2 = random.Next(1, 10);
+        exp = random.Next(0, 4);
+        if (exp == 2)
+        {
+            while (!(var1 % var2 == 0 && var1 > var2))
+            {
+                var1 = random.Next(1, 10);
+                var2 = random.Next(1, 10);
+            }
+        }
+        else if (exp == 3)
+        {
+            while (!(var1 < 5 && var2 < 5))
+            {
+                var1 = random.Next(1, 10);
+                var2 = random.Next(1, 10);
+            }
+        }
+        else
+        {
+            while (var1 < var2)
+            {
+                var1 = random.Next(1, 10);
+                var2 = random.Next(1, 10);
+            }
+        }
+
         this.QuizVar1.Text = var1.ToString();
         this.QuizVar2.Text = var2.ToString();
-        this.QuizExpression.Text = allExpressions[0].ToString();
+        this.QuizExpression.Text = allExpressions[exp].ToString();
 
+        return var1 + allExpressions[exp].ToString() + var2;
     }
 
     void giveOptions()
@@ -45,10 +85,40 @@ public partial class MathQuiz : ContentPage
         this.opt1 = random.Next(1, 20);
         this.opt2 = random.Next(1, 20);
         this.opt3 = random.Next(1, 20);
+
         this.Option1.Text = opt1.ToString();
         this.Option2.Text = opt2.ToString();
         this.Option3.Text = opt3.ToString();
+        return;
+
     }
+
+    int evaluateExpression(String str)
+    {
+        int i = 0;
+        if (exp == 0)
+        {
+            i = var1 + var2;
+            return i;
+        }
+        else if (exp == 1)
+        {
+            i = var1 - var2;
+            return i;
+        }
+        else if (exp == 2)
+        {
+            i = var1 / var2;
+            return i;
+        }
+        else if (exp == 3)
+        {
+            i = var1 * var2; ;
+            return i;
+        }
+        return i;
+    }
+
     void visibleGame(int a)
     {
         if (a == 1)
@@ -60,8 +130,6 @@ public partial class MathQuiz : ContentPage
             visibleSkip(1);
             visibleStartButton(0);
             visibleTryAgain(1);
-            visibleCorrect(0);
-            visibleIncorrect(0);
         }
         else
         {
@@ -72,8 +140,6 @@ public partial class MathQuiz : ContentPage
             visibleSkip(0);
             visibleStartButton(1);
             visibleTryAgain(0);
-            visibleCorrect(0);
-            visibleIncorrect(0);
         }
 
     }
@@ -154,26 +220,5 @@ public partial class MathQuiz : ContentPage
             this.Skip.IsVisible = false;
         }
     }
-    void visibleCorrect(int a)
-    {
-        if (a == 1)
-        {
-            this.CorrectAnswer.IsVisible = true;
-        }
-        else
-        {
-            this.CorrectAnswer.IsVisible = false;
-        }
-    }
-    void visibleIncorrect(int a)
-    {
-        if (a == 1)
-        {
-            this.IncorrectAnswer.IsVisible = true;
-        }
-        else
-        {
-            this.IncorrectAnswer.IsVisible = false;
-        }
-    }
+    
 }
